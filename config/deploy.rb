9fd0d9008db2adc2296da49b50b8a89f5f70dc09
cabanga <manucabanga@gmail.com>
2023-10-29 18:32:29 +0100

    chore: Config assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.18.0"
 
-set :application, "sempre_chique"
-set :repo_url, "git@github.com:cabanga/sempre-chique-portal.git"
+set :application, "jk_grupo"
+set :repo_url, "git@github.com:moxi-group/jk-grupo-portal.git"
 set :deploy_to, "/opt/projects/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
